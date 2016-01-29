3c149e51e24013dbe5ed5e7503f5cf24d7d1df69
simba_huang <simba_huang@unication.com.cn>
2016-01-29 18:06:08 +0800

    add 4.7

--- ch4.lisp
+++ ch4.lisp
@@ -275,3 +275,141 @@
 (make-point)
 ;; The function print-point will display points in an abbreviated form.
 
+;; 4.7 Example: Binary Search Trees
+;;                 (5) 
+;;                /  \
+;;              (4)   (8)
+;;              /     / \
+;;            (2)   (6)  (9)
+;;            / \     \
+;;          (1) (3)   (7)
+;;         Figure 4.4: A binary search tree.
+(defstruct (node (:print-function
+                  (lambda (n s d)
+                    (format s "#<~A>" (node-elt n)))))
+  elt (l nil) (r nil))
+
+(defun bst-insert (obj bst <)
+  (if (null bst)
+      (make-node :elt obj)
+      (let ((elt (node-elt bst)))
+        (if (eql obj elt)
+            bst
+            (if (funcall < obj elt)
+                (make-node
+                 :elt elt
+                 :l (bst-insert obj (node-l bst) <)
+                 :r (node-r bst))
+                (make-node
+                 :elt elt
+                 :r (bst-insert obj (node-r bst) <)
+                 :l (node-l bst)))))))
+
+(defun bst-find (obj bst <)
+  (if (null bst)
+      nil
+      (let ((elt (node-elt bst)))
+        (if (eql obj elt)
+            bst
+            (if (funcall < obj elt)
+                (bst-find obj (node-l bst) <)
+                (bst-find obj (node-r bst) <))))))
+
+(defun bst-min (bst)
+  (and bst
+       (or (bst-min (node-l bst)) bst)))
+
+(defun bst-max (bst)
+  (and bst
+       (or (bst-max (node-r bst)) bst)))
+
+(setf nums nil)
+(dolist (x '(5 8 4 2 1 9 6 7 3))
+  (setf nums (bst-insert x nums #'<)))
+(bst-find 12 nums #'<)
+(bst-find 4 nums #'<)
+(bst-min nums)
+(bst-max nums)
+
+(defun bst-remove (obj bst <)
+  (if (null bst)
+      nil
+      (let ((elt (node-elt bst)))
+        (if (eql obj elt)
+            (percolate bst)
+            (if (funcall < obj elt)
+                (make-node
+                 :elt elt
+                 :l (bst-remove obj (node-l bst) <)
+                 :r (node-r bst))
+                (make-node
+                 :elt elt
+                 :r (bst-remove obj (node-r bst) <)
+                 :l (node-l bst)))))))
+
+(defun percolate (bst)
+  (cond ((null (node-l bst))
+         (if (null (node-r bst))
+             nil
+             (rperc bst)))
+        ((null (node-r bst)) (lperc bst))
+        (t (if (zerop (random 2)) 
+               (lperc bst)
+               (rperc bst)))))
+;; The expression (random 2) will return either 0 or 1, so (zerop (random 2))
+;; will return true half the time.
+
+(defun rperc (bst)
+  (make-node :elt (node-elt (node-r bst))
+             :l (node-l bst)
+             :r (percolate (node-r bst))))
+
+(defun lperc (bst)
+  (make-node :elt (node-elt (node-l bst))
+             :r (node-r bst)
+             :l (percolate (node-l bst))))
+
+(setf nums (bst-remove 4 nums #'<))
+(bst-find 4 nums #'<)
+
+;; Binary Search Trees: Traversal
+(defun bst-traverse (fn bst)
+  (when bst
+    (bst-traverse fn (node-l bst))
+    (funcall fn (node-elt bst))
+    (bst-traverse fn (node-r bst))))
+
+(bst-traverse #'princ nums)
+
+;; 4.8 Hash Table
+(setf ht (make-hash-table))
+(gethash 'color ht)
+(setf (gethash 'color ht) 'red)
+(setf (gethash 'name ht) nil)
+
+(setf bugs (make-hash-table))
+(push "Doesn't take keyworkd arguments."
+      (gethash #'our-member bugs))
+;; Since gethash returns nil by default, and push is an abbreviation for a
+;; setf, we can simply push new strings into the entry for a function.
+(gethash #'our-member bugs)
+
+;; Since gethash returns nil by default, a new-mode hash table is also,
+;; conveniently, an empty set.
+;; To remove an object from a set, you would call remhash, chich removes
+;; an entry from a hash table:
+(setf fruit (make-hash-table))
+(setf (gethash 'apricot fruit) t)
+(gethash 'apricot fruit)
+(remhash 'apricot fruit)
+;; The return value shows whether there was an entry to remove;
+
+;; There is an iteration function for hash tables: maphash, which takes
+;; a function of two arguments and a hash table. The function will be
+;; called on every key/value pair in the table, in no particular order:
+(setf (gethash 'shape ht) 'spherical
+      (gethash 'size ht) 'giant)
+
+(maphash #'(lambda (k v)
+             (format t "~A = ~A~%" k v))
+         ht)
